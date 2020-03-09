31faf2da182e18b6578c58f38baced28946a6d18
Fafanur <senolfatmanur@gmail.com>
2020-03-09 20:22:15 +0100

    Staff movement

---
+++ Periode3_EindProject_FatmanurSenol/Assets/_Scripts/ObjectHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHolder : MonoBehaviour
+{
+    public GameObject targetObject;
+    public Vector3 offset;
+    public Vector3 offsetRot;
+    public float hoverSpeed;
+    public float hoverDistance;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+    void FixedUpdate()
+    {
+        if (targetObject != null)
+        {
+            Vector3 targetPos = transform.position + offset.x * transform.right + (offset.y + (Mathf.Sin(Time.time * hoverSpeed) * hoverDistance)) * transform.up + offset.z * transform.forward;
+            Vector3 targetRot = transform.rotation.eulerAngles + offsetRot;
+            targetObject.transform.position = Vector3.Lerp(targetObject.transform.position, targetPos, moveSpeed * Time.fixedDeltaTime);
+            targetObject.transform.rotation = Quaternion.Lerp(targetObject.transform.rotation, Quaternion.Euler(targetRot), rotationSpeed * Time.fixedDeltaTime ); 
+            
+        }
+        
+    }
+}
